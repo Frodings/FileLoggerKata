6a77436db02a85b3834a56464d65dc3e1b6299d0
Frode Selnes Haugarne <34833156+Frodings@users.noreply.github.com>
2022-02-12 21:29:24 +0100

    Add project files.

---
+++ FileLoggerKata/FileLogger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoggerKata
+{
+    public class FileLogger
+    {
+
+        private readonly IFileWrapper _fileWrapper;
+        private readonly IDateTimeWrapper _dateTimeWrapper;
+               
+        public FileLogger(IFileWrapper fileWrapper, IDateTimeWrapper dateTimeWrapper)
+        {
+            _fileWrapper = fileWrapper;
+            _dateTimeWrapper = dateTimeWrapper;
+        }
+
+        public void Log(string message)
+        {
+            string text = $"{_dateTimeWrapper.GetNowString()} {message}";
+            string filePath = GetCurrentLogFilePath();
+
+            _fileWrapper.AppendText(filePath, text);
+        }
+
+        private string GetCurrentLogFilePath()
+        {
+            string currentDate = _dateTimeWrapper.GetNowDateString();
+            string fileName = $"log{currentDate}.txt";
+
+            return fileName;
+        }
+    }
+
+    public interface IFileWrapper
+    {
+        void AppendText(string filePath, string text);
+    }
+
+    // TODO klasse som implementerer dette interfacet må også testes
+    // det må testes at strengene returneres på korrekt format
+
+    public interface IDateTimeWrapper
+    {
+        string GetNowString();//DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+
+        string GetNowDateString();//DateTime.Now.TosTring(yyyyMMdd)
+    }
+
+    //public class FileWrapper : IFileWrapper
+    //{
+    //    private readonly string _path;
+
+    //    public FileWrapper(string filePath)
+    //    {
+    //        _path = filePath;
+    //    }
+
+    //    public void AppendText(string text)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //}
+}
---
+++ FileLoggerKata/FileLoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace FileLoggerKata
+{
+    [TestFixture]
+    public class FileLoggerTests
+    {
+
+        [Test]
+        public void Log_RandomMessage_WritesMessage()
+        {
+            // arrange
+            //stub datetime
+            IDateTimeWrapper stubDateTime = Substitute.For<IDateTimeWrapper>();
+            stubDateTime.GetNowString().Returns("2022-02-12 08:09:01");
+            stubDateTime.GetNowDateString().Returns("20220212");
+
+            IFileWrapper mockFileWrapper = Substitute.For<IFileWrapper>();
+
+            // act
+            var logger = new FileLogger(mockFileWrapper, stubDateTime);
+            logger.Log("Dette er en test");
+
+            //assert
+            // assert against mock, that the method was called with expected text
+
+            mockFileWrapper.Received().AppendText("log20220212.txt", "2022-02-12 08:09:01 Dette er en test");
+        }
+    }
+}
