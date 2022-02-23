d2142dc9ba57fd71e387f61cb761a3a761b826b6
Frode Selnes Haugarne <34833156+Frodings@users.noreply.github.com>
2022-02-23 16:35:45 +0100

    Step 8 done

--- FileLoggerKata/FileLogger.cs
+++ FileLoggerKata/FileLogger.cs
@@ -9,6 +9,7 @@ namespace FileLoggerKata
 {
     public class FileLogger
     {
+        private readonly string _weekendFile = "weekend.txt";
 
         private readonly IFileWrapper _fileWrapper;
         private readonly IDateTimeWrapper _dateTimeWrapper;
@@ -24,46 +25,80 @@ namespace FileLoggerKata
             string text = $"{_dateTimeWrapper.GetNowString()} {message}";
             string filePath = GetCurrentLogFilePath();
 
+            if (filePath == _weekendFile)
+                RenameWeekendFileIfNecessary();
+
             _fileWrapper.AppendText(filePath, text);
         }
 
         private string GetCurrentLogFilePath()
         {
-            string currentDate = _dateTimeWrapper.GetNowDateString();
-            string fileName = $"log{currentDate}.txt";
-
+            string fileName;
+            if (!_dateTimeWrapper.NowIsWeekend())
+            {
+                string currentDate = _dateTimeWrapper.GetNowDateString();
+                fileName = $"log{currentDate}.txt";
+            }
+            else
+            {
+                fileName = _weekendFile;
+            }
+            
             return fileName;
+        }
+
+        /// <summary>
+        /// Checks if weekend file exist and when its modified - If its not modified current weekend, it is renamed.
+        /// </summary>
+        private void RenameWeekendFileIfNecessary()
+        {
+            if (_fileWrapper.FileExist(_weekendFile))
+            {
+                DateTime modifiedDate = _fileWrapper.GetFileModifiedDate(_weekendFile);
+                TimeSpan interval = _dateTimeWrapper.GetNow().Date - modifiedDate.Date;
+
+                if (interval.TotalDays >= 2) // not modified this weekend
+                {
+                    string newFileName;
+                    if (modifiedDate.DayOfWeek == DayOfWeek.Saturday)
+                        newFileName = $"weekend-{modifiedDate.ToString("yyyyMMdd")}.txt";
+                    else
+                    {
+                        DateTime saturday = modifiedDate.AddDays(- ((int)modifiedDate.DayOfWeek + 1));
+                        newFileName = $"weekend-{saturday.ToString("yyyyMMdd")}.txt";
+                    }
+                        
+                    _fileWrapper.RenameFile(_weekendFile, newFileName);
+                }
+            }
         }
     }
 
     public interface IFileWrapper
     {
         void AppendText(string filePath, string text);
+
+        bool FileExist(string filepath);
+
+        DateTime GetFileModifiedDate(string filePath);
+
+        bool RenameFile(string srcFilePath, string newFilePath);
     }
 
-    // TODO klasse som implementerer dette interfacet må også testes
+    // TODO en klasse som implementerer dette interfacet må også testes
     // det må testes at strengene returneres på korrekt format 
 
     public interface IDateTimeWrapper
     {
+        DateTime GetNow();
+
         string GetNowString();//DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
 
         string GetNowDateString();//DateTime.Now.TosTring(yyyyMMdd)
+
+        bool NowIsWeekend();
+
     }
 
-    //public class FileWrapper : IFileWrapper
-    //{
-    //    private readonly string _path;
-
-    //    public FileWrapper(string filePath)
-    //    {
-    //        _path = filePath;
-    //    }
-
-    //    public void AppendText(string text)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //}
+    
 }
--- FileLoggerKata/FileLoggerTests.cs
+++ FileLoggerKata/FileLoggerTests.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
 using NSubstitute;
+using NUnit.Framework;
 
 namespace FileLoggerKata
 {
@@ -12,25 +8,90 @@ namespace FileLoggerKata
     public class FileLoggerTests
     {
 
+        private FileLogger CreateFileLogger(DateTime logTime, bool logTimeIsWeekend, IFileWrapper fileWrapper)
+        {
+            IDateTimeWrapper stubDateTime = Substitute.For<IDateTimeWrapper>();
+            stubDateTime.GetNowString().Returns(logTime.ToString("yyyy-MM-dd HH:mm:ss"));
+            stubDateTime.GetNowDateString().Returns(logTime.ToString("yyyyMMdd"));
+            stubDateTime.NowIsWeekend().Returns(logTimeIsWeekend);
+            stubDateTime.GetNow().Returns(logTime);
+
+            var logger = new FileLogger(fileWrapper, stubDateTime);
+
+            return logger;
+        }
+
         [Test]
-        public void Log_RandomMessage_WritesMessage()
+        public void Log_WhenDayIsWeekday_WritesMessage()
         {
             // arrange
-            //stub datetime
-            IDateTimeWrapper stubDateTime = Substitute.For<IDateTimeWrapper>();
-            stubDateTime.GetNowString().Returns("2022-02-12 08:09:01");
-            stubDateTime.GetNowDateString().Returns("20220212");
-
             IFileWrapper mockFileWrapper = Substitute.For<IFileWrapper>();
+            var testTime = new DateTime(2022, 02, 12, 8, 9, 1);
+            FileLogger logger = CreateFileLogger(testTime, false, mockFileWrapper);
 
             // act
-            var logger = new FileLogger(mockFileWrapper, stubDateTime);
             logger.Log("Dette er en test");
 
-            //assert
-            // assert against mock, that the method was called with expected text
-
-            mockFileWrapper.Received().AppendText("log20220212.txt", "2022-02-12 08:09:01 Dette er en test");
+            //assert - bruker NSubstitute sin Received() for å sjekke at mockFileWrapper.AppendText() er kalt opp som angitt 
+            string expectedFileName = $"log{testTime.ToString("yyyyMMdd")}.txt";
+            string expectedLogMessage = $"{testTime.ToString("yyyy-MM-dd HH:mm:ss")} Dette er en test";
+            mockFileWrapper.Received().AppendText(expectedFileName, expectedLogMessage);
         }
+
+        [Test]
+        public void Log_WhenDayIsWeekend_LogFileNameIsWeekend()
+        {
+            IFileWrapper mockFileWrapper = Substitute.For<IFileWrapper>();
+            var testTime = new DateTime(2022, 2, 13, 21, 21, 1);
+            var logger = CreateFileLogger(testTime, true, mockFileWrapper);
+
+            logger.Log("Dette er en test");
+
+            mockFileWrapper.Received().AppendText("weekend.txt", "2022-02-13 21:21:01 Dette er en test");
+        }
+
+        // ! Det er kanskje feil navngivning på disse metodene, siden det er Log() som faktisk kalles
+        // og dette er inngangen for Unit under test..
+        // navn kunne vært Log_CallsRenameFile_
+        [TestCase("2022-02-13 18:13:01", "2022-02-06 21:15:13", "weekend-20220205.txt")]
+        [TestCase("2022-02-12 18:13:01", "2022-01-01 15:06:22", "weekend-20220101.txt")]
+        public void RenameWeekendFileIfNecessary_WhenFileIsOld_RenameFileIsCalled(DateTime logTime, DateTime exisitingFileModifiedDate, string expectedFileName)
+        {
+            IFileWrapper mockFileWrapper = Substitute.For<IFileWrapper>();
+
+            mockFileWrapper.GetFileModifiedDate(Arg.Any<string>()).Returns(exisitingFileModifiedDate);
+            mockFileWrapper.FileExist(Arg.Any<string>()).Returns(true);
+
+            FileLogger logger = CreateFileLogger(logTime, true, mockFileWrapper);
+            logger.Log("Dette er en test");
+
+            mockFileWrapper.Received().RenameFile(Arg.Any<string>(), expectedFileName);
+        }
+
+
+        [TestCase("2022-02-13 14:04:01", "2022-02-12 21:32:22")]
+        public void RenameWeekendFileIfNecessary_WhenFileIsNew_RenameFileIsNotCalled(DateTime logTime, DateTime existingFileModifiedDate)
+        {
+            IFileWrapper mockFileWrapper = Substitute.For<IFileWrapper>();
+            mockFileWrapper.GetFileModifiedDate(Arg.Any<string>()).Returns(existingFileModifiedDate);
+            mockFileWrapper.FileExist(Arg.Any<string>()).Returns(true);
+
+            FileLogger logger = CreateFileLogger(logTime, true, mockFileWrapper);
+            logger.Log("Dette er en test");
+
+            mockFileWrapper.DidNotReceive().RenameFile(Arg.Any<string>(), Arg.Any<string>());
+        }
+    
     }
+
+    // TODO
+    // lag klasse for IDateTimeWrapper
+    // test denne
+
+    //public void NowIsWeekend_WhenNowIsNotWeekend_ReturnsFalse
+    //public void NowIsWeekend_WhenNowIsWeekend_ReturnsTrue
+
+    //Hvordan assert mot mock vha NSubstitute
+    //-bruke Received() for å sjekke at angitt metode kalles opp som forventet
+    //mockObject.Received().MetodeSomSkalSjekkes([forventede parametere])
 }
